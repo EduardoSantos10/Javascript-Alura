05c092c566abbf56859c4b8e8f175dfa2d75648b
Eduardo <eduardo25082002@gmail.com>
2025-01-18 10:28:52 -0300

    Primeiro commit

---
+++ app.js
@@ -0,0 +1,12 @@
+alert('Boas Vindas Rapaziada!'); // passar uma mensagem
+
+numeroSec = 20;
+
+let chute = prompt('Escolha um número entre 1 e 30');
+
+if(chute == numeroSec){
+    console.log('Isso ai, você descobriu o número secreto (20)!')
+}
+else{
+    console.log('Você errou o número!')
+}
