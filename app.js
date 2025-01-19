cefa502c296d45bef101dd173550a889edfe2b82
Eduardo <eduardo25082002@gmail.com>
2025-01-19 18:10:58 -0300

    Terceiro Commit

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 alert('Boas Vindas Rapaziada!'); // passar uma mensagem
-
 numeroSec = 20;
 console.log(numeroSec);
 
-let chute = prompt('Escolha um número entre 1 e 30');
+let chute;
+let tentativas = 1;
 
-if(chute == numeroSec){
-    alert(`Isso ai, você descobriu o número secreto, pois ele é: ${numeroSec}`);m // ${} concatenção
+
+// Enquanto
+while(chute != numeroSec){
+    chute = prompt('Escolha um número entre 1 e 30');
+
+    if(chute == numeroSec){
+        alert(`Isso ai, você descobriu o número secreto, pois ele é: ${numeroSec} com ${tentativas} tentativas`);m // ${} concatenção
+    }
+    else{
+        if(chute > numeroSec){
+            alert(`O número secreto é menor que o ${chute}`);
+        }else{
+            alert(`O número secreto é maior que o ${chute}`);
+        }
+        // tentativas = tentativas + 1;
+        tentativas++;
+    }
 }
-else{
-    alert('Você errou o número!');
-}
+
